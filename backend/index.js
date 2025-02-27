81beefe94288415a8b6a709ae4c1585be009f37c
Aniruddha Raut <aniruddharautofficial@gmail.com>
2025-02-27 22:03:58 +0530

    Setup AuthMiddleware

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
+
+
 const mainRouter = require("./routes/index.js")
 
 const app = express();
 const port = process.env.port || 3000;
-
-app.use(cors());
-app.use(express.json());
 
 app.use("/api/v1", mainRouter)
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,56 @@
 const express = require("express")
+const zod = require("zod")
+const jwt = require("jsonwebtoken")
+
+const {User} = require("../db.js")
+const JWT_SECRET = require("../config")
 
 const router = express.Router()
+
+const signupSchema = zod.object({
+  username: zod.string(),
+  password: zod.string(),
+})
 
 router.get('/', (req, res) => {
   res.send('User route at /api/v1/user')
 })
+
+router.post('/signin', async (req, res) => {
+
+  res.send("/api/v1/user/signin")
+  const body = req.body;
+  const obj = signupSchema.safeParse(req.body);
+
+  if (!obj.success) {
+    return res.json({message:"Incorrect inputs"})
+  }
+
+  const user = User.findOne({
+    username: body.username
+  })
+
+  if (user._id) {
+    return res.json({
+      message: "User Already Exists."
+    })
+  }
+
+  const dbUser = await User.create(body);
+  const token = jwt.sign({
+    userId: dbUser._id
+  }, JWT_SECRET)
+
+  res.json({
+    message: "User created successfully.",
+    token: token
+  })
+
+})
+
+router.post('/signup', (req, res) => {
+  res.send("/api/v1/user/signup")
+})
+
 
 module.exports = router;
